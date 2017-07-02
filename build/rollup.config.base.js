5a8e70d0b5dedfc697f94247a4f86645c9eb0aa3
toxic-johann <353904974@qq.com>
2017-07-02 15:55:32 +0800

    [first blood] add some function what: why: how:

---
+++ build/rollup.config.base.js
@@ -0,0 +1,62 @@
+const {version, name, author, license} = require('../package.json');
+const banner = `
+/**
+ * ${name} v${version}
+ * (c) 2017 ${author}
+ * Released under ${license}
+ */
+`;
+import flow from 'rollup-plugin-flow-no-whitespace';
+import includePaths from 'rollup-plugin-includepaths';
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+const babelConfig = {
+  common: {
+    presets: [
+      'flow',
+      ['latest', {es2015: {modules: false}}]
+    ],
+    runtimeHelpers: true,
+    babelrc: false
+  },
+  es: {
+    presets: [
+      'flow',
+      ['latest', {es2015: {modules: false}}]
+    ],
+    runtimeHelpers: true,
+    babelrc: false
+  },
+  umd: {
+    presets: ['flow', 'es2015-rollup'],
+    plugins: [],
+    babelrc: false
+  },
+  iife: {
+    presets: ['flow', 'es2015-rollup'],
+    plugins: [],
+    babelrc: false
+  },
+  min: {
+    presets: ['flow', 'es2015-rollup'],
+    plugins: [],
+    babelrc: false
+  }
+};
+export default function (mode) {
+  return {
+    entry: 'src/index.js',
+    banner,
+    plugins: [
+      babel(babelConfig[mode]),
+      flow(),
+      resolve(),
+      includePaths({
+        include: {},
+        paths: ['src'],
+        external: mode === 'min' ? [] : ['toxic-predicate-functions'],
+        extensions: ['.js']
+      })
+    ]
+  };
+};
