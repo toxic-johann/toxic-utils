8ea9994552940ca36d3035c6780f0eea8d9242ff
toxic-johann <353904974@qq.com>
2017-07-02 18:46:22 +0800

    [build] 0.1.1 what: add babel-runtime as external why: how:

--- build/rollup.config.base.js
+++ build/rollup.config.base.js
@@ -7,15 +7,17 @@ const banner = `
  */
 `;
 import flow from 'rollup-plugin-flow-no-whitespace';
-import includePaths from 'rollup-plugin-includepaths';
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 const babelConfig = {
   common: {
     presets: [
       'flow',
       ['latest', {es2015: {modules: false}}]
     ],
+    plugins: ['transform-runtime'],
+    exclude: 'node_modules/**',
     runtimeHelpers: true,
     babelrc: false
   },
@@ -24,12 +26,16 @@ const babelConfig = {
       'flow',
       ['latest', {es2015: {modules: false}}]
     ],
+    plugins: ['transform-runtime'],
+    exclude: 'node_modules/**',
     runtimeHelpers: true,
     babelrc: false
   },
   umd: {
     presets: ['flow', 'es2015-rollup'],
-    plugins: [],
+    plugins: ['transform-runtime'],
+    exclude: 'node_modules/**',
+    runtimeHelpers: true,
     babelrc: false
   },
   iife: {
@@ -47,16 +53,14 @@ export default function (mode) {
   return {
     entry: 'src/index.js',
     banner,
+    external (id) {
+      return !/min|umd|iife/.test(mode) && /babel-runtime|toxic-predicate-functions/.test(id);
+    },
     plugins: [
       babel(babelConfig[mode]),
       flow(),
       resolve(),
-      includePaths({
-        include: {},
-        paths: ['src'],
-        external: mode === 'min' ? [] : ['toxic-predicate-functions'],
-        extensions: ['.js']
-      })
+      commonjs()
     ]
   };
 };
